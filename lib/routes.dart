e7e091b5caab555264077707f2e84dfd876293c9
Iskander2323 <iska200314@gmail.com>
2025-04-08 14:10:05 +0500

    new route for form

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,13 @@
 import 'package:go_router/go_router.dart';
 import 'package:to_do_app/components/ui/home_page.dart';
+import 'package:to_do_app/components/ui/to_do_form_page.dart';
 
-final GoRouter routes = GoRouter(
-  initialLocation: '/home',
-  routes: [
-    GoRoute(path: '/home', builder: (context, state) => HomePage(),)
-  ]);
+final GoRouter routes = GoRouter(initialLocation: '/home', routes: [
+  GoRoute(path: '/home', builder: (context, state) => HomePage(), routes: [
+    GoRoute(
+      path: '/to_do_form_page',
+      name: 'to_do_form_page',
+      builder: (context, state) => ToDoFormPage(),
+    )
+  ])
+]);
