301dd4f56b9c09748d98cb5ba0112c6d42f99acf
Iskander2323 <iska200314@gmail.com>
2025-04-09 15:08:48 +0500

    added from local constructor

--- lib/components/data/model/to_do_model.dart
+++ lib/components/data/model/to_do_model.dart
@@ -4,22 +4,31 @@ import 'package:to_do_app/components/local_db/local_db.dart';
 
 class ToDoModel extends Equatable {
   const ToDoModel(
-      {required this.title,
+      {required this.id,
+      required this.title,
       this.description,
       required this.createdAt,
       required this.isDone});
-
+  final int id;
   final String title;
   final String? description;
-  final DateTime createdAt;
+  final DateTime? createdAt;
   final bool isDone;
+
+  ToDoModel.fromLocal(ToDoItem toDoItem)
+      : id = toDoItem.id,
+        title = toDoItem.title,
+        description = toDoItem.description,
+        createdAt = toDoItem.createdAt,
+        isDone = toDoItem.isDone;
 
   ToDoItemsCompanion toDoItemsCompanion() {
     final titleCompanion = Value<String>(title);
     final descriptionCompanion = Value<String?>(description);
-    final createdAtCompanion = Value<DateTime>(createdAt);
+    final createdAtCompanion = Value<DateTime?>(createdAt);
     final isDoneCompanion = Value<bool>(isDone);
     return ToDoItemsCompanion(
+      id: Value<int>(0),
       title: titleCompanion,
       description: descriptionCompanion,
       createdAt: createdAtCompanion,
@@ -28,5 +37,5 @@ class ToDoModel extends Equatable {
   }
 
   @override
-  List<Object?> get props => [title, description, createdAt, isDone];
+  List<Object?> get props => [id, title, description, createdAt, isDone];
 }
