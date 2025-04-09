cf08a8fbac52d7f942ea9f6bad20ba6e14255fe8
Iskander2323 <iska200314@gmail.com>
2025-04-09 15:09:26 +0500

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:to_do_app/components/ui/home_page.dart';
-import 'package:to_do_app/components/ui/to_do_form_page.dart';
+import 'package:to_do_app/components/ui/form_to_do_page/form_to_do_page.dart';
 
 final GoRouter routes = GoRouter(initialLocation: '/home', routes: [
   GoRoute(path: '/home', builder: (context, state) => HomePage(), routes: [
